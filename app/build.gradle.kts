33393f101da7ccb6001b6069222ed0345ba41011
Tobe Iheme <ihemeprogress@gmail.com>
2024-07-01 19:48:50 +0100

    designed bottom navigation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,16 +1,18 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.plugin.serialization")
+    id("org.jetbrains.kotlin.plugin.compose")
 }
 
 android {
     namespace = "com.example.koseemani"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.koseemani"
         minSdk = 26
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -21,6 +23,10 @@ android {
     }
 
     buildTypes {
+
+        debug {
+
+        }
         release {
             isMinifyEnabled = false
             proguardFiles(
@@ -39,9 +45,9 @@ android {
     buildFeatures {
         compose = true
     }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.4.3"
-    }
+//    composeOptions {
+//        kotlinCompilerExtensionVersion = "1.4.3"
+//    }
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
@@ -51,14 +57,20 @@ android {
 
 dependencies {
 
+
+
+    implementation ("androidx.navigation:navigation-compose:2.8.0-alpha08")
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.2")
     implementation("androidx.activity:activity-compose:1.8.2")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation("androidx.compose.ui:ui")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+
+    implementation("androidx.navigation:navigation-runtime-ktx:2.7.7")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
---
+++ app/src/main/java/com/example/koseemani/navigation/routes.kt
@@ -0,0 +1,14 @@
+package com.example.koseemani.navigation
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+object Home
+
+@Serializable
+object Contacts
+@Serializable
+object Settings
+
+@Serializable
+object History
