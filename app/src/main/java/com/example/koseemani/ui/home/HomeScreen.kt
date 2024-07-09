78d9fb2eb55ffdc8d0dc6c292109522d32d8b4f9
Tobe Iheme <ihemeprogress@gmail.com>
2024-07-09 18:49:24 +0100

    implemented live location

--- app/src/main/java/com/example/koseemani/ui/home/HomeScreen.kt
+++ app/src/main/java/com/example/koseemani/ui/home/HomeScreen.kt
@@ -1,8 +1,16 @@
 package com.example.koseemani.ui.home
 
 import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Address
+import android.location.Geocoder
+import android.location.LocationManager
+import androidx.activity.compose.ManagedActivityResultLauncher
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresPermission
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -25,12 +33,24 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.ArrowForward
 import androidx.compose.material.icons.outlined.Notifications
 import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.SnackbarResult
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldColors
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -47,31 +67,90 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.platform.LocalContext
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.content.ContextCompat.getSystemService
+
 import androidx.navigation.ActivityNavigatorExtras
 import com.example.koseemani.R
 import com.example.koseemani.utils.SMSManager
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.MultiplePermissionsState
 import com.google.accompanist.permissions.isGranted
+import com.google.accompanist.permissions.rememberMultiplePermissionsState
 import com.google.accompanist.permissions.rememberPermissionState
 import com.google.accompanist.permissions.shouldShowRationale
+import com.google.android.gms.location.LocationServices
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.util.Locale
 
-@OptIn(ExperimentalPermissionsApi::class)
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalPermissionsApi::class, ExperimentalMaterial3Api::class)
 @Composable
-fun HomeScreen(modifier: Modifier = Modifier) {
+fun HomeScreen(
+    modifier: Modifier = Modifier,
+    snackbarHostState: SnackbarHostState
+) {
+    val scope = rememberCoroutineScope()
+    val context = LocalContext.current
+    var isPermitted by remember {
+        mutableStateOf(false)
+    }
     val smsPermissionState = rememberPermissionState(Manifest.permission.SEND_SMS)
-    val requestPermissionLauncher = rememberLauncherForActivityResult(contract = ActivityResultContracts.RequestPermission(), onResult = {isGranted->
-        if(isGranted){
-            SMSManager.sendSOSMessage("SOS, I am in danger", "08102309062")
+    val locationPermissionState = rememberMultiplePermissionsState(
+        permissions = listOf(
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.SEND_SMS
+        )
+    )
+    val requestPermissionLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.RequestPermission(),
+        onResult = { isGranted ->
+            if (isGranted) {
+                SMSManager.sendSOSMessage("SOS, I am in danger", "08102309062")
 
-        }else{
+            } else {
 
 
+            }
+
+        })
+   val locationPermissionLauncher = rememberLauncherForActivityResult(
+        ActivityResultContracts.RequestMultiplePermissions()
+    ) { permissions ->
+        when {
+         permissions.getOrDefault(Manifest.permission.ACCESS_FINE_LOCATION, false) -> {
+                isPermitted = true
+//                requestPermissionLauncher.launch(Manifest.permission.SEND_SMS)
+//                smsPermissionState.launchPermissionRequest()
+            }
+
+            permissions.getOrDefault(Manifest.permission.ACCESS_COARSE_LOCATION, false) -> {
+                // Only approximate location access granted.
+            }
+
+            permissions.getOrDefault(Manifest.permission.SEND_SMS, defaultValue = false) -> {
+                SMSManager.sendSOSMessage("SOS, I am in danger", "08102309062")
+            }
         }
+    }
 
-    })
+//  checkLocationPermission(locationPermissionState)
 
-
-
+//    locationPermissionRequest.launch(arrayOf(
+//        Manifest.permission.ACCESS_FINE_LOCATION,
+//        Manifest.permission.ACCESS_COARSE_LOCATION))
+//    handleLocationPermissions(locationPermissionState) {
+//        snackbarImpl(it, scope, snackbarHostState){
+//            locationPermissionState.launchMultiplePermissionRequest()
+//        }
+//    }
 
 
     val safetyList = listOf<SafetyInsightItem>(
@@ -96,6 +175,33 @@ fun HomeScreen(modifier: Modifier = Modifier) {
         Spacer(modifier = Modifier.height(16.dp))
         NameHeadline(userName = "Fisayo")
         Spacer(modifier = Modifier.height(24.dp))
+
+
+        when (PackageManager.PERMISSION_GRANTED) {
+            ContextCompat.checkSelfPermission(
+                context,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) -> {
+                CurrentLocationField(locationPermissionsState = locationPermissionState)
+            }
+            else -> {
+                // You can directly ask for the permission.
+                // The registered ActivityResultCallback gets the result of this request.
+                SideEffect{
+                    locationPermissionLauncher.launch(arrayOf(
+                        Manifest.permission.ACCESS_FINE_LOCATION,
+                        Manifest.permission.ACCESS_COARSE_LOCATION,
+                       ))
+                }
+                }
+
+        }
+
+
+
+
+
+        Spacer(modifier = Modifier.height(32.dp))
         InstructionText(
             modifier = Modifier
                 .padding(vertical = 8.dp)
@@ -110,7 +216,20 @@ fun HomeScreen(modifier: Modifier = Modifier) {
                 .align(alignment = Alignment.CenterHorizontally)
                 .size(160.dp)
                 .clickable(onClick = {
-                    requestPermissionLauncher.launch(Manifest.permission.SEND_SMS)
+                    if (locationPermissionState.allPermissionsGranted) {
+//                        requestPe
+                    } else {
+                        handleLocationPermissions(locationPermissionState) {
+                            snackbarImpl(it, scope, snackbarHostState) {
+                                locationPermissionLauncher.launch(arrayOf(
+                                    Manifest.permission.ACCESS_FINE_LOCATION,
+                                    Manifest.permission.ACCESS_COARSE_LOCATION,
+                                ))
+
+                            }
+                        }
+                    }
+
                 }
                 )
 
@@ -128,6 +247,7 @@ fun HomeScreen(modifier: Modifier = Modifier) {
 
     }
 }
+
 
 @Composable
 fun NameHeadline(modifier: Modifier = Modifier, userName: String) {
@@ -190,6 +310,74 @@ fun InstructionText(modifier: Modifier = Modifier) {
             append("contacts")
         }
     })
+}
+
+
+@RequiresPermission(anyOf = [Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION])
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalPermissionsApi::class)
+@Composable
+fun CurrentLocationField(
+    modifier: Modifier = Modifier,
+    locationPermissionsState: MultiplePermissionsState
+) {
+    val scope = rememberCoroutineScope()
+    val context = LocalContext.current
+    val locationClient = remember {
+        LocationServices.getFusedLocationProviderClient(context)
+    }
+    val locationEnabled = isLocationEnabled(context)
+    var locationInfo by remember {
+        mutableStateOf("")
+    }
+
+    if (locationEnabled) {
+        scope.launch(Dispatchers.IO) {
+            locationClient.lastLocation.addOnSuccessListener { location ->
+                locationInfo = if (location == null) {
+                    "No last known location. Try fetching the current location first"
+                } else {
+                    val geocoder = Geocoder(context, Locale.getDefault())
+                    val list: MutableList<Address>? =
+                        geocoder.getFromLocation(location.latitude, location.longitude, 1)
+                    list?.get(0)?.getAddressLine(0) ?: "No location found"
+
+                }
+
+            }
+        }
+    } else {
+        locationInfo = "Please enable device location to see current location"
+    }
+
+    TextField(
+        modifier = modifier.fillMaxWidth(),
+        value = locationInfo,
+        onValueChange = {},
+        readOnly = true,
+        leadingIcon = {
+            Icon(
+                painter = painterResource(id = R.drawable.location_pic),
+                contentDescription = "Map Icon"
+            )
+        },
+        isError = !locationEnabled,
+
+        textStyle = MaterialTheme.typography.titleSmall,
+        colors = TextFieldDefaults.textFieldColors(
+            containerColor = MaterialTheme.colorScheme.surface,
+            textColor = Color(0x99000000)
+        )
+    )
+
+}
+
+private fun isLocationEnabled(context: Context): Boolean {
+
+    val locationManager: LocationManager =
+        context.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+    return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(
+        LocationManager.NETWORK_PROVIDER
+    )
 }
 
 @Composable
@@ -263,15 +451,79 @@ fun SafetyInsightCard(modifier: Modifier = Modifier, safetyItem: SafetyInsightIt
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun HomeScreenPreview() {
-    HomeScreen(
-        modifier = Modifier
-            .fillMaxWidth()
-            .fillMaxHeight()
-    )
+//    HomeScreen(
+//        modifier = Modifier
+//            .fillMaxWidth()
+//            .fillMaxHeight()
+//    )
 }
 
 @OptIn(ExperimentalPermissionsApi::class)
-fun sendSms(){
+
+fun snackbarImpl(
+    message: String,
+    scope: CoroutineScope,
+    snackbarHostState: SnackbarHostState,
+
+    onActionClicked: () -> Unit,
+) {
+
+    scope.launch {
+        val result = snackbarHostState.showSnackbar(
+            message = message,
+            actionLabel = "Request Permission",
+            duration = SnackbarDuration.Indefinite
+        )
+
+        when (result) {
+            SnackbarResult.ActionPerformed -> {
+                onActionClicked()
+
+            }
+
+            SnackbarResult.Dismissed -> {
+                /* Handle snackbar dismissed */
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalPermissionsApi::class)
+fun checkLocationPermission(locationPermissionState: MultiplePermissionsState) {
+    locationPermissionState.launchMultiplePermissionRequest()
+}
+
+@OptIn(ExperimentalPermissionsApi::class)
+fun handleLocationPermissions(
+    locationPermissionsState: MultiplePermissionsState,
+    showSnackBar: (String) -> Unit
+) {
+    val allPermissionsRevoked =
+        locationPermissionsState.permissions.size ==
+                locationPermissionsState.revokedPermissions.size
+
+    if (!allPermissionsRevoked) {
+        // If not all the permissions are revoked, it's because the user accepted the COARSE
+        // location permission, but not the FINE one.
+        showSnackBar(
+            "Yay! Thanks for letting me access your approximate location. " +
+                    "But you know what would be great? If you allow me to know where you " +
+                    "exactly are. Thank you!"
+        )
+
+    } else if (locationPermissionsState.shouldShowRationale) {
+        // Both location permissions have been denied
+
+        showSnackBar(
+            "Getting your exact location is important for this app. " +
+                    "Please grant us fine location. Thank you :D"
+        )
+    } else if (locationPermissionsState.revokedPermissions.size == locationPermissionsState.permissions.size) {
+        // First time the user sees this feature or the user doesn't want to be asked again
+
+        showSnackBar("This app requires location permission")
+    }
+
 
 }
 
